32d4f3357c08a79b5764dc83ef480db2728d506f
garrip91 <45920127+garrip91@users.noreply.github.com>
2021-02-21 01:22:09 +0300

    Summary

--- mainapp/models.py
+++ mainapp/models.py
@@ -145,8 +145,10 @@ class Smartphone(Product):
     resolution = models.CharField(max_length=255, verbose_name="Разрешение экрана")
     accum_volume = models.CharField(max_length=255, verbose_name="Объём батареи")
     ram = models.CharField(max_length=255, verbose_name="Оперативная память")
-    sd = models.BooleanField(default=True)
-    sd_volume_max = models.CharField(max_length=255, verbose_name="Максимальный объём встраиваемой памяти")
+    sd = models.BooleanField(default=True, verbose_name="Наличие SD-карты")
+    sd_volume_max = models.CharField(
+        max_length=255, null=True, blank=True, verbose_name="Максимальный объём встраиваемой памяти"
+    )
     main_cam_mp = models.CharField(max_length=255, verbose_name="Главная камера")
     frontal_cam_mp = models.CharField(max_length=255, verbose_name="Фронтальная камера")
     
@@ -155,6 +157,12 @@ class Smartphone(Product):
         
     def get_absolute_url(self):
         return get_product_url(self, 'product_detail')
+        
+    # @property
+    # def sd(self):
+        # if self.sd:
+            # return "Да!"
+        # return "Нет!"
         
         
         
--- mainapp/templatetags/specifications.py
+++ mainapp/templatetags/specifications.py
@@ -2,6 +2,8 @@ from django import template
 
 from django.utils.safestring import mark_safe
 
+from mainapp.models import Smartphone
+
 
 
 register = template.Library()
@@ -66,4 +68,9 @@ def get_product_spec(product, model_name):
 @register.filter
 def product_spec(product):
     model_name = product.__class__._meta.model_name
+    if isinstance(product, Smartphone):
+        if not product.sd:
+            PRODUCT_SPEC['smartphone'].pop('Максимальный объём встраиваемой памяти')
+        else:
+            PRODUCT_SPEC['smartphone']['Максимальный объём встраиваемой памяти'] = 'sd_volume_max'
     return mark_safe(TABLE_HEAD + get_product_spec(product, model_name) + TABLE_TAIL)
